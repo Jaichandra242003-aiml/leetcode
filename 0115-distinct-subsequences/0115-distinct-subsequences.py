402ff88af86d02ec6b7a0ab8df65351fb39f55cd
Jaya Chandra Dadi <112895292+JayaChandraDadi@users.noreply.github.com>
2026-09-05 22:25:30 -0500

    Time: 507 ms (28.15%), Space: 28.5 MB (66.03%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,24 +1,19 @@
-class Solution(object):
-    def count(self,s,t,i,j,dp):
-        if j==0:
+class Solution:
+    def helper(self,s,t,i,j,dp):
+        if j==len(t):
             return 1
-        if i==0:
+        if i==len(s):
             return 0
         if dp[i][j]!=-1:
             return dp[i][j]
-        if s[i-1]==t[j-1]:
-            dp[i][j]=self.count(s,t,i-1,j-1,dp)+self.count(s,t,i-1,j,dp)
+        if s[i]==t[j]:
+            take = self.helper(s,t,i+1,j+1,dp)
+            skip = self.helper(s,t,i+1,j,dp)
+            ans = take + skip
         else:
-            dp[i][j] = self.count(s,t,i-1,j,dp)
-        return dp[i][j]
-    def numDistinct(self, s, t):
-        n1 = len(s)
-        n2 = len(t)
-        prev = [0]*(n2+1) 
-        prev[0] = 1
-        for i in range(1,n1+1):
-            for j in range(n2,0,-1):
-                if s[i-1]==t[j-1]:
-                    prev[j]=prev[j-1]+prev[j]
-        return prev[n2]
-        
+            ans = self.helper(s,t,i+1,j,dp)
+        dp[i][j]  = ans
+        return ans
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = [[-1 for _ in range(len(t)+1)]for _ in range(len(s)+1)]
+        return self.helper(s,t,0,0,dp)
